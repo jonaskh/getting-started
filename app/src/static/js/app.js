be644ba74565898f50161f41fc4788edbcdb9336
jonaskh <jonaskh@stud.ntnu.no>
2022-11-15 01:44:45 +0100

    changed button text to test pipeline

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add'}
+                        {submitting ? 'Adding...' : 'Add item to list'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
